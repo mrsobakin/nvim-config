eeeefdb8df5b50d8cfda79ce516e4bc3264b7f81
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2025-05-10 09:27:25 +0300

    feat: remove `knap` plugin

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -64,7 +64,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 
-vim.keymap.set({ "n", "v", "i" }, "<F5>", function() require("knap").process_once() end)
-vim.keymap.set({ "n", "v", "i" }, "<F6>", function() require("knap").close_viewer() end)
-vim.keymap.set({ "n", "v", "i" }, "<F7>", function() require("knap").toggle_autopreviewing() end)
-vim.keymap.set({ "n", "v", "i" }, "<F8>", function() require("knap").forward_jump() end)
