e07ff4c390bb50cbfff265565664f59509b0ca92
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2025-05-10 08:33:24 +0300

    feat: ditch bufferline, use lualine instead

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,39 @@
 return { 
-	"nvim-lualine/lualine.nvim",
-	config={
-		options={
-			component_separators = { left = '', right = ''},
-			section_separators = { left = '', right = ''},
-		}
-	}
+    "nvim-lualine/lualine.nvim",
+    config={
+        options = {
+            refresh = {
+                tabline = 30,
+            },
+            component_separators = { left = '/', right = '/'},
+            section_separators = { left = '', right = ''},
+        },
+        sections = {
+            lualine_c = {
+                {
+                    'filename',
+                    path=1
+                },
+            },
+        },
+        tabline = {
+            lualine_a = {
+                {
+                    "buffers",
+                    mode = 0,
+                    max_length = function()
+                        return vim.o.columns
+                    end,
+                    -- colored_icons = false,
+                    buffers_color = {
+                        active = {fg='black', bg='LightGrey'},
+                        inactive = {fg='LightGrey', bg='black'},
+                    },
+                    symbols = {
+                        alternate_file = '',
+                    },
+                },
+            },
+        },
+    },
 }
