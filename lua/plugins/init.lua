109280f00fb582e224310b6ea8854c7ea6b92c81
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2025-05-10 09:23:35 +0300

    feat: remove `trouble.nvim` and `nvim-luapad`

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,8 @@
 return {
     { "ethanholz/nvim-lastplace" },
-    { "folke/trouble.nvim" },
     { "j-hui/fidget.nvim", opts={} },
     { "kylechui/nvim-surround", opts={} },
     { "neovim/nvim-lspconfig" },
     { "numToStr/Comment.nvim" },
     { "nvim-treesitter/nvim-treesitter" },
-    { "rafcamlet/nvim-luapad" },
 }
