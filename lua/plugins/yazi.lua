59482df2ce8f692a588c316866ac0ae4d5c44633
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2025-05-10 08:42:53 +0300

    feat: add yazi plugin

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,22 @@
+return {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = { "folke/snacks.nvim", lazy = true },
+    keys = {
+        {
+            "-",
+            mode = { "n", "v" },
+            "<cmd>Yazi<cr>",
+            desc = "Open yazi at the current file",
+        },
+    },
+    opts = {
+        open_for_directories = true,
+        keymaps = {
+            show_help = "<f1>",
+        },
+    },
+    init = function()
+        vim.g.loaded_netrwPlugin = 1
+    end,
+}
